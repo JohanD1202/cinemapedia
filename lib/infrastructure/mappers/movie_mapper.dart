acccdbeb0d6709774ed0827b06468972bb98b609
Johan Idárraga <idarragajohan8@gmail.com>
2026-01-10 00:11:18 -0500

    Add movies search bar

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -2,13 +2,16 @@ import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
 
+
 class MovieMapper {
+  static const String noPosterUrl = 'https://upload.wikimedia.org/wikipedia/commons/0/0a/No-image-available.png';
+  static const String urlBase = 'https://image.tmdb.org/t/p/w500/';
 
   static Movie movieDBToEntity(MovieMovieDB moviedb) => Movie(
     adult: moviedb.adult,
     backdropPath: (moviedb.backdropPath != '')
-      ? 'https://image.tmdb.org/t/p/w500/${moviedb.backdropPath}'
-      : 'https://www.pngkey.com/png/detail/147-1473883_404-error-404-not-found-png.png',
+      ? '$urlBase${moviedb.backdropPath}'
+      : noPosterUrl,
     genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
     id: moviedb.id,
     originalLanguage: moviedb.originalLanguage,
@@ -16,9 +19,9 @@ class MovieMapper {
     overview: moviedb.overview,
     popularity: moviedb.popularity,
     posterPath: (moviedb.posterPath != '')
-      ? 'https://image.tmdb.org/t/p/w500/${moviedb.posterPath}'
-      : 'no-poster',
-    releaseDate: moviedb.releaseDate,
+      ? '$urlBase${moviedb.posterPath}'
+      : noPosterUrl,
+    releaseDate: moviedb.releaseDate != null ? moviedb.releaseDate! : DateTime.now(),
     title: moviedb.title,
     video: moviedb.video,
     voteAverage: moviedb.voteAverage,
@@ -28,8 +31,8 @@ class MovieMapper {
   static Movie movieDetailsToEntity(MovieDetails moviedb) => Movie(
     adult: moviedb.adult,
     backdropPath: (moviedb.backdropPath != '')
-      ? 'https://image.tmdb.org/t/p/w500/${moviedb.backdropPath}'
-      : 'https://www.pngkey.com/png/detail/147-1473883_404-error-404-not-found-png.png',
+      ? '$urlBase${moviedb.backdropPath}'
+      : noPosterUrl,
     genreIds: moviedb.genres.map((e) => e.name).toList(),
     id: moviedb.id,
     originalLanguage: moviedb.originalLanguage,
@@ -37,8 +40,8 @@ class MovieMapper {
     overview: moviedb.overview,
     popularity: moviedb.popularity,
     posterPath: (moviedb.posterPath != '')
-      ? 'https://image.tmdb.org/t/p/w500/${moviedb.posterPath}'
-      : 'no-poster',
+      ? '$urlBase${moviedb.posterPath}'
+      : noPosterUrl,
     releaseDate: moviedb.releaseDate,
     title: moviedb.title,
     video: moviedb.video,
